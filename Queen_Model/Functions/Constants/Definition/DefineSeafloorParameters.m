57cfe8a43bb77eb06815a9bf21caff7269b07c33
= <rossdmw@gmail.com>
2018-08-29 15:02:24 +0100

    Updated the default initial parameters for the seafloor by testing various options with model runs. Specifically the downgoing speed has been decreased (so that the timescale burial to subduction is better) and there are default core depths specified.

--- Queen_Model/Functions/Constants/Definition/DefineSeafloorParameters.m
+++ Queen_Model/Functions/Constants/Definition/DefineSeafloorParameters.m
@@ -2,9 +2,9 @@ function Seafloor = DefineSeafloorParameters(Seafloor);
     %% Seafloor Parameters
     Seafloor.Subduction_Mean = 8000;
     Seafloor.Subduction_Spread = 500;
-    Seafloor.Subduction_Rate = [ones(1050,1)*(00/(100*10^6))/10;ones(951,1)*10000/(100*10^6)]; %m/Ma
+    Seafloor.Subduction_Rate = [zeros(1050,1);ones(950,1)*3.3333e-5;0]; %m/Ma
     Seafloor.Subduction_Risk = 0.001;
     Seafloor.Uplift_Rate = zeros(2001,1);
     Seafloor.Uplift_Rate(2:1050) = 1e-7;
-    Seafloor.Core_Depths = NaN;
+    Seafloor.Core_Depths = [10000;8000;5000;3000;2000;1000;0;-1000;-2000;-3000;-5000;-8000;-10000];
 end
